ad446e92e6b297cc797db6862e33ab79028b6567
golobor <goloborodko.anton@gmail.com>
2017-04-30 16:59:35 -0400

    add docker build/publish scripts

---
+++ docker/build.sh
@@ -0,0 +1,16 @@
+set -ex
+
+USERNAME=mirnylab
+IMAGE=distiller_env
+
+cp  ../VERSION ./VERSION
+cp ../environment.yml ./environment.yml
+
+function cleanup {
+    rm  ./VERSION
+    rm  ./environment.yml
+}
+
+trap cleanup EXIT
+
+docker build -t $USERNAME/$IMAGE:latest .
