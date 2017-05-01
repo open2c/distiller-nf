a09a3b3bc3a8350699a994dd29af312ab2ef532e
golobor <goloborodko.anton@gmail.com>
2017-04-30 21:38:42 -0400

    store the test dataset on github (mirnylab/distiller-test-data)

--- setup_test.sh
+++ setup_test.sh
@@ -1,28 +1,8 @@
+set -ex
+
 PROJECT_DIR=$(pwd)/test
-N_LINES=10000
 
-mkdir -p ${PROJECT_DIR}/genome
-cd ${PROJECT_DIR}/genome
-wget http://hgdownload-test.cse.ucsc.edu/goldenPath/sacCer3/bigZips/sacCer3.chrom.sizes
-wget http://hgdownload-test.cse.ucsc.edu/goldenPath/sacCer3/bigZips/chromFa.tar.gz
-{ tar -O -xf chromFa.tar.gz && rm chromFa.tar.gz; } | bgzip -c > sacCer3.fa.gz
-bwa index sacCer3.fa.gz
-
-mkdir -p ${PROJECT_DIR}/fastq/MATalpha_R1/lane1/
-mkdir -p ${PROJECT_DIR}/fastq/MATalpha_R1/lane2/
-mkdir -p ${PROJECT_DIR}/fastq/MATalpha_R2/lane1/
-mkdir -p ${PROJECT_DIR}/fastq/MATa_R1/lane1/
-mkdir -p ${PROJECT_DIR}/fastq/MATa_R2/lane1/
-
-
-cd ${PROJECT_DIR}/fastq/MATalpha_R1/lane1/
-fastq-dump -F -X ${N_LINES} SRR2601842 --split-files --gzip
-cd ${PROJECT_DIR}/fastq/MATalpha_R1/lane2/
-fastq-dump -F -X ${N_LINES} SRR2601843 --split-files --gzip
-cd ${PROJECT_DIR}/fastq/MATalpha_R2/lane1/
-fastq-dump -F -X ${N_LINES} SRR2601845 --split-files --gzip
-cd ${PROJECT_DIR}/fastq/MATa_R1/lane1/
-fastq-dump -F -X ${N_LINES} SRR2601848 --split-files --gzip
-cd ${PROJECT_DIR}/fastq/MATa_R2/lane1/
-fastq-dump -F -X ${N_LINES} SRR2601851 --split-files --gzip
-
+mkdir -p ${PROJECT_DIR}
+cd ${PROJECT_DIR}
+curl -LkSs https://api.github.com/repos/mirnylab/distiller-test-data/tarball \
+    | tar -zxf - --strip=1 --wildcards '*/genome/*' --wildcards '*/fastq/*'
