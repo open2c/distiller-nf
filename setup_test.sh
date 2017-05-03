142d39426f2b80af2c053146fdb3e97f078270a2
golobor <goloborodko.anton@gmail.com>
2017-05-03 14:29:42 -0400

    try a cross-platform version of setup_test.sh

--- setup_test.sh
+++ setup_test.sh
@@ -1,8 +1,38 @@
+#!/bin/bash    
+
+# The goal of this script is to download data files hosted an a github repo.
+# The script has to work both on Linux and MacOS so we could not use some
+# nice Linux-specific features of tar and instead had to manually extract
+# necessary files via a temp folder.
 set -ex
 
 PROJECT_DIR=$(pwd)/test
-
 mkdir -p ${PROJECT_DIR}
-cd ${PROJECT_DIR}
+
+TMPDATADIR=`mktemp -d 2>/dev/null || mktemp -d -t 'TMPDATADIR'`
+
+# check if tmp dir was created
+if [[ ! "$TMPDATADIR" || ! -d "$TMPDATADIR" ]]; then
+    echo "Could not create temp dir"
+    exit 1
+fi
+
+# deletes the temp directory at exit
+function cleanup {      
+    rm -rf "$TMPDATADIR"
+    echo "Deleted temp directory $TMPDATADIR"
+}
+
+trap cleanup EXIT
+
+
+cd ${TMPDATADIR}
 curl -LkSs https://api.github.com/repos/mirnylab/distiller-test-data/tarball \
-    | tar -zxf - --strip=1 --wildcards '*/genome/*' --wildcards '*/fastq/*'
+    | tar -zxf - 
+# cd to the first (and the only) folder that was extracted from the tarball
+cd $(ls -d */|head -n 1)
+mv -n ./genome ${PROJECT_DIR}
+mv -n ./fastq ${PROJECT_DIR}
+
+cd ${PROJECT_DIR}
+
