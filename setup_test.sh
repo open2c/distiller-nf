08724b2050a0162fecce427cf7c419fa4ca415a0
golobor <goloborodko.anton@gmail.com>
2018-11-09 12:51:21 -0500

    try another fix for setup_test.sh

--- setup_test.sh
+++ setup_test.sh
@@ -27,7 +27,8 @@ trap cleanup EXIT
 
 
 cd ${TMPDATADIR}
-curl -LkSs https://api.github.com/repos/mirnylab/distiller-test-data/tarball | tar -zxf - 
+#curl -LkSs https://api.github.com/repos/mirnylab/distiller-test-data/tarball | tar -zxf - 
+wget -qO- https://api.github.com/repos/mirnylab/distiller-test-data/tarball | tar xvz
 # cd to the first (and the only) folder that was extracted from the tarball
 cd $(ls -d */|head -n 1)
 mv -n ./genome ${PROJECT_DIR}
