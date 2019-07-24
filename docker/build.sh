ff8e294f976ef013f4e7dc6aecfe07ac9f9d6fbd
Nezar <nabdennur@gmail.com>
2019-07-24 18:16:25 -0400

    Playing more with paths

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,24 @@ set -ex
 
 USERNAME=mirnylab
 IMAGE=distiller_env
+version=$(cat ../VERSION | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')
 
-docker build -t $USERNAME/$IMAGE:latest docker
-docker run -it mirnylab/distiller_env:latest apt list | sed 's/\x1b\[[0-9;]*m//g' > ./apt.list
-docker run -it mirnylab/distiller_env:latest conda list > ./conda.list
+
+cp  ../VERSION ./VERSION
+cp ../environment.yml ./environment.yml
+
+function cleanup {
+    rm  ./VERSION
+    rm  ./environment.yml
+}
+
+trap cleanup EXIT
+
+# bop it
+docker build -t $USERNAME/$IMAGE:latest . 
+docker run -it mirnylab/distiller_env:latest apt list | sed 's/\x1b\[[0-9;]*m//g' | tee ./apt.list
+docker run -it mirnylab/distiller_env:latest conda list | tee ./conda.list
+docker images
+
+# tag it
+docker tag mirnylab/distiller_env:latest mirnylab/distiller_env:$version
--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,13 +1,5 @@
 set -ex
 
-version=$(cat ./VERSION | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')
-docker run -it mirnylab/distiller_env:latest apt list | sed 's/\x1b\[[0-9;]*m//g'
-docker run -it mirnylab/distiller_env:latest conda list
-docker images
-
-# tag it
-docker tag mirnylab/distiller_env:latest mirnylab/distiller_env:$version
-
 # push it
 docker push mirnylab/distiller_env:latest
 docker push mirnylab/distiller_env:$version
