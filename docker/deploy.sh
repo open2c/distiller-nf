433e5f84d9875833544d1def9d18404604375d95
Nezar <nabdennur@gmail.com>
2019-07-24 16:40:47 -0400

    Mock out real stuff

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,7 +1,8 @@
-set -euxo pipefail
+docker login -u mirnylab -p $DOCKER_PASSWORD
 
-echo "$DOCKER_PASSWORD" | docker login -u mirnylab --password-stdin
+set -ex
 
+version=$(cat ../VERSION | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')
 cp  ../VERSION ./VERSION
 cp ../environment.yml ./environment.yml
 
@@ -12,18 +13,17 @@ function cleanup {
 
 trap cleanup EXIT
 
-# bop it
-docker build -t mirnylab/distiller_env:latest .
-docker run -it mirnylab/distiller_env:latest apt list | sed 's/\x1b\[[0-9;]*m//g'
-docker run -it mirnylab/distiller_env:latest conda list
-docker images
+# # bop it
+# docker build -t mirnylab/distiller_env:latest .
+# docker run -it mirnylab/distiller_env:latest apt list | sed 's/\x1b\[[0-9;]*m//g'
+# docker run -it mirnylab/distiller_env:latest conda list
+# docker images
 
-# tag it
-version=$(cat ../VERSION | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')
 echo $version
 
-docker tag mirnylab/distiller_env:latest mirnylab/distiller_env:$version
+# # tag it
+# docker tag mirnylab/distiller_env:latest mirnylab/distiller_env:$version
 
-# push it
-docker push mirnylab/distiller_env:latest
-docker push mirnylab/distiller_env:$version
+# # push it
+# docker push mirnylab/distiller_env:latest
+# docker push mirnylab/distiller_env:$version
