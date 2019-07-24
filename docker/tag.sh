a9a966ffc1c9757fdcc0cfdcdbeddb53e517e13f
Nezar <nabdennur@gmail.com>
2019-07-24 18:59:37 -0400

    Split build, tag and deploy

--- docker/tag.sh
+++ docker/tag.sh
@@ -3,6 +3,5 @@ set -ex
 version=$(cat ../VERSION | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')
 echo "version: $version"
 
-# push it
-docker push mirnylab/distiller_env:latest
-docker push mirnylab/distiller_env:$version
+# tag it
+docker tag mirnylab/distiller_env:latest mirnylab/distiller_env:$version
